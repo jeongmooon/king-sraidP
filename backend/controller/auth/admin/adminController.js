ef5dcacde5c16775be8a7bd23297a6e6fd492735
jeong moon <ijhghji@gmail.com>
2021-12-27 10:26:03 +0900

    로그인,회원가입완료

--- backend/controller/auth/admin/adminController.js
+++ backend/controller/auth/admin/adminController.js
@@ -1,8 +1,9 @@
 const adminModels = require('../../../models/auth/admin');
 const passwordModule = require('../../../modules/passwrodModule');
-const {OK, DB_ERROR,OVER_ACCOUNT} = require('../../../modules/stautsModule')
+const {OK, DB_ERROR,OVER_ACCOUNT,NO_ACCOUNT, LOCK_ACCOUNT, OVER_PASSWORD} = require('../../../modules/stautsModule')
 
 const adminController ={
+    // 회원가입
     createAdmin : async(req, res)=>{
         const {adminId, email, password} = req.body;
 
@@ -43,6 +44,84 @@ const adminController ={
             data.save();
             res.status(OK).json({
                 message:"회원 가입 완료"
+            })
+        }
+    },
+
+    // 로그인
+    loginAdmin : async(req, res)=>{
+        const {adminId, password} =req.body
+
+        // 아이디 있나 체크
+        let loginId;
+        try {
+            loginId = await adminModels.findOne({adminId})
+        } catch (error) {
+            res.status(DB_ERROR).json({
+                message:"DB에러로 ID확인 실패"
+            })
+        }
+
+        if(loginId){
+            //아이디가 있다면 잠금확인
+            if(loginId.lockAccount === false){
+                const loginPassword = loginId.password;
+                const objId = loginId._id;
+                const salt = loginId.salt;
+
+                // 비밀번호 다시 해시화
+                const passwordP = passwordModule.createHashedPassword(password, salt);
+
+                // 비밀번호 비교
+                if(loginPassword === passwordP){
+                    // 비밀번호가 같다면 토큰생성
+                    res.status(OK).json({
+                        message:"로그인 성공"
+                    })
+                }else{
+                    // 비밀번호가 틀리다면
+                    if(loginId.signinCount > 9){
+                        // 잠금카운트가 9이상이라면
+                        try {
+                            await adminModels.updateOne(
+                                {adminId},
+                                {$set:{lockAccount : true}}
+                            )
+                            res.status(LOCK_ACCOUNT).json({
+                                message:"비밀번호가 10회이상틀려 잠김"
+                            })
+                        } catch (error) {
+                            res.status(DB_ERROR).json({
+                                message:"비밀번호 잠금체크 DB오류"
+                            })
+                        }
+                    } else{
+                        // 잠금이 10회 이하라면
+                        try {
+                            await adminModels.updateOne(
+                                {adminId},
+                                {$set : {signinCount:loginId.signinCount + 1}}
+                            )
+                            res.status(OVER_PASSWORD).json({
+                                message:"비밀번호 틀렸음"
+                            })
+                        } catch (error) {
+                            res.status(DB_ERROR).json({
+                                message:"비밀번호카운트증가 DB에러"
+                            })
+                        }
+                    }
+                }
+            }else{
+                // 잠금상태다
+                res.status(LOCK_ACCOUNT).json({
+                    message:"계정이 잠겨있다"
+                })
+            }
+        } else{
+            //아이디가 없다
+            res.status(NO_ACCOUNT).json({
+                message:"아이디가 없습니다"
             })
         }
     }
--- backend/modules/stautsModule.js
+++ backend/modules/stautsModule.js
@@ -1,6 +1,9 @@
 const statusModule ={
     OK : 200,
     OVER_ACCOUNT : 400,
+    OVER_PASSWORD : 401,
+    LOCK_ACCOUNT : 402,
+    NO_ACCOUNT : 403,
     DB_ERROR : 500
 }
 
