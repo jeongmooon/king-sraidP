e15e03c896b84f19d0758db0b62cbb269c2f70de
jeong moon <ijhghji@gmail.com>
2022-01-17 23:48:28 +0900

    채팅

--- frontend/src/containers/main/chat/ChatContainer.jsx
+++ frontend/src/containers/main/chat/ChatContainer.jsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ChatComponent from '../../../components/main/chat/ChatComponent'
+import client from '../../../libs/api/client'
 
 function ChatContainer() {
+    const [dataInput, setDataInput] = useState()
+
+    const onClickChat = () =>{
+        const res = client.post("/", dataInput)
+    }
     return (
         <ChatComponent />
     )
